e2b8439c5b93b0ab710e848a20166c50cff2a1d0
John MacAulay <merjohn@bigpond.net.au>
2021-03-03 11:29:24 +1100

    Added location to experiences via migrations

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -52,7 +52,7 @@ class ExperiencesController < ApplicationController
   end
 
   def experience_params
-    params.require(:experience).permit(:title, :description, :unit_current_price, photos: [])
+    params.require(:experience).permit(:title, :description, :unit_current_price, :location, photos: [])
   end
 
 end
