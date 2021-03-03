d2508b3ddd75e57d1487cca877f4cef5101a5915
damian lonel <damian@playworkstudios.com.au>
2021-03-03 15:35:27 +1100

    nav away

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,10 @@ class DashboardsController < ApplicationController
 
 def show
   @experiences = current_user.experiences
+
+  # @booked_experiences = Bookings.find(params[@bookings.experience_id])
   @bookings = current_user.bookings
+
 end
 
 end
