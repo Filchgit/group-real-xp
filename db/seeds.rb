123add3f6f67d87d10b84d5bf463140e844ee974
John MacAulay <merjohn@bigpond.net.au>
2021-03-03 13:30:30 +1100

    want seeds changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,3 @@
 user = User.create(email: "user1@user.com", password: "password" )
 
 
-Experience.create!(
-user_id: user.id,
-title: "Scuba Diving",
-description: "Best Scuba class in Melbourne",
-unit_current_price: "300"
-  )
-Experience.create!(
-user_id: user.id,
-title: "Sky Diving",
-description: "Best Sky diving class in Melbourne",
-unit_current_price: "2000"
-  )
-Experience.create!(
-user_id: user.id,
-title: "Deep Sea Diving",
-description: "Best Deep Sea Diving class in Melbourne",
-unit_current_price: "4000"
-  )
